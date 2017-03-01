9bbff4c6dba7f689a5ca5004ecf4e001ca1af0c0
kylejneuner <kn4uz@virginia.edu>
2017-02-28 19:29:17 -0500

    attempted to connect php and front end

--- html/addSong.php
+++ html/addSong.php
@@ -1,7 +1,12 @@
-<?
-  require ('checkID.php');
-
+<?php
+  require ('dbconnect.php');
+  $flag['songAdded']=0;
   $songid = $_POST['song'];
+  $roomid = $_POST['room'];
   $addsong = mysql_query("INSERT INTO songs (songid,roomid) VALUES(\"$songid\",\"$roomid\")") or die(mysql_error());
+  if($addsong){
+    $flag['songAdded']=1;
+  }
+  print(json_encode($flag));
 
 ?>
--- html/retrieveSong.php
+++ html/retrieveSong.php
@@ -1,12 +1,7 @@
 <?
   require ('dbconnect.php');
 
-  if(empty($_SERVER['REQUEST_URI'])) {
-      $_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'];
-  }
-  // Strip off query string so dirname() doesn't get confused
-  $url = preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']);
-  $folderpath = 'http://'.$_SERVER['HTTP_HOST'].'/'.ltrim(dirname($url), '/').'/';
+
 
   $roomid=$_REQUEST['roomid'];
 
